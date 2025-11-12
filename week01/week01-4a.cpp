10b4de156a025885c14f7a1d7750a820ad193aef
Squidpenguin <941230.aptx4869@gmail.com>
2025-11-12 11:55:50 +0800

    week01

---
+++ week01/week01-4a.cpp
@@ -0,0 +1,13 @@
+//week01-4a.cpp
+#include <stdio.h>
+int main()
+{
+	int a,b,ans=0;
+	scanf("%d%d",&a,&b);
+	for (int i=a; i<=b; i++){
+		if (i%3 == 0){
+			ans += i;
+		}
+	}
+	printf("%d",ans);
+}
